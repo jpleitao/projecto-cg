e8e137cd2025d5ed7800ff9ce03ce4c85a1b2b8f
João Ricardo Lourenço <jorl17.8@gmail.com>
2014-06-13 15:24:59 +0100

    Implement the new renderer

--- Renderer.cxx
+++ Renderer.cxx
@@ -1,10 +1,11 @@
 #include "Renderer.h"
 
-Renderer::Renderer() :
+Renderer::Renderer(mat4 P, mat4 V) :
     program("data/shaders/TransformVertexShader.vertexshader", "data/shaders/ColorFragmentShader.fragmentshader") {
 
     modelVertexHandle = program.getAttrib("vertexPosition_modelspace");    
     vertexColorHandle = program.getAttrib("vertexColor");
+    this->P = P; this->V = V; calculatePV();
 }
 
 void Renderer::setCurrentModelMatrix(mat4 M) {
@@ -17,7 +18,9 @@ ShaderProgram* Renderer::getCurrentProgram() {
 }
 
 void Renderer::render(std::vector<Object*> objects) {
-    for (int i = 0; i < objects.size(); i++) {
+    program.use();
+    for (int i = 0; i < objects.size(); i++) {        
         objects.at(i)->render(this);
     }
+    program.stopUsing();
 }
--- Renderer.h
+++ Renderer.h
@@ -28,7 +28,12 @@ class Renderer
     //FIXME: Might add other handles    
 
     public:
-        Renderer();
+        Renderer(mat4 P = glm::perspective(45.0f, 4.0f / 3.0f, 0.1f, 100.0f),
+                mat4 V       = glm::lookAt(
+                                vec3(4,3,-3), // Camera is at (4,3,-3), in World Space
+                                vec3(0,0,0), // and looks at the origin
+                                vec3(0,1,0))  // Head is up (set to 0,-1,0 to look upside-down)
+        );
 
         void setProjection(mat4 P) { this->P = P; calculatePV();}
         void setView(mat4 V) { this->V = V; calculatePV();}
