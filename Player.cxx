15ff060663e13ea62991acc55fbc789bc3e9ae15
João Ricardo Lourenço <jorl17.8@gmail.com>
2014-06-14 22:43:13 +0100

    Limit vertical angle. Don't let horizontal angle overflow

--- Player.cxx
+++ Player.cxx
@@ -3,12 +3,18 @@
 
 Player::Player(Renderer* renderer, vec3 position, float horizAngle, float vertAngle, float fov, float speed, float mouseSpeed) :
     renderer(renderer), position(position), horizAngle(horizAngle), vertAngle(vertAngle), fov(fov), speed(speed), mouseSpeed(mouseSpeed) {
-
+        updateViewMatrix();
 }
 
 void Player::updateAngles(float screendx, float screendy) {
     horizAngle      += mouseSpeed * screendx;
     vertAngle       += mouseSpeed * screendy;
+
+    if ( vertAngle < -M_PI/2 ) vertAngle = -M_PI/2;
+    else if ( vertAngle > M_PI/2 ) vertAngle = M_PI/2;
+
+    if ( horizAngle > 2*M_PI ) horizAngle -= 2*M_PI;
+    else if ( horizAngle < -2*M_PI ) horizAngle += 2*M_PI;
 
     updateViewMatrix();
 }
@@ -18,8 +24,12 @@ void Player::updateViewMatrix(double xoff, double yoff) {
     vec3 right     = vec3( sin(horizAngle - M_PI/2.0f),  0, cos(horizAngle - M_PI/2.0f) );    
     vec3 up        = glm::cross( right, direction );
 
-    position += direction * (float)yoff;
-    position += right * (float)xoff;
+    if ( yoff)
+        position += direction * (float)yoff;
+
+    if ( xoff)
+        position += right * (float)xoff;
+    printf("player at (%f,%f,%f), horiz=%f, vert=%f\n", position.x, position.y, position.z, horizAngle, vertAngle);
 
     renderer->setView(glm::lookAt(  position,            // He's here
                                     position+direction, // and looks here : at the same position, plus "direction"
