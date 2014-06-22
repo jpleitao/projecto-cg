ebe66b5d9524d8f9611b70a3c924866a1e593c95
joaosimoes <joaomiguel_condeixa@hotmail.com>
2014-06-22 02:16:35 +0100

    World implementation

---
+++ World.h
@@ -0,0 +1,21 @@
+#ifndef WORLD_H
+
+#include "Renderer.h"
+#include "ModelManager.h"
+#include <stdio.h>
+#include <iostream>
+
+#define WORLD_H
+
+
+class World
+{
+    std::vector<Object*> worldObjects;
+    Renderer* renderer;
+
+public:
+    World(ModelManager* modelManager, Renderer* renderer, const char* filename);
+
+};
+
+#endif
