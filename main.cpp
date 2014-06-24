4285cb183ee5997f3ec152f334f6e095bdc8da13
joaosimoes <joaomiguel_condeixa@hotmail.com>
2014-06-24 19:11:34 +0100

    Boxes and bounding boxes

--- main.cpp
+++ main.cpp
@@ -89,7 +89,7 @@ int main (void) {
 
     int frameNo = 0;
 
-    Light blueLight(vec3(0,0,0), vec3(0,0,0));
+    Light blueLight(vec3(0,0,0), vec3(1,1,1));
     renderer.addLight(&blueLight);
     Light redLightFromAbove(vec3(0,2,0), vec3(1,0,0));
     renderer.addLight(&redLightFromAbove);
